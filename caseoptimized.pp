af783e7e4e21f32ad3fb525e1a6916c2b7ccec5f
roybhaskar9 <bhaskar@study9.com>
2017-09-03 10:18:33 +0530

    Update caseoptimized.pp

--- caseoptimized.pp
+++ caseoptimized.pp
@@ -3,3 +3,8 @@ centos, redhat: { $apache = "httpd" }
 debian, ubuntu: { $apache = "apache2" }
 default: { notify {"Unrecognized operating system for webserver": } }
 }
+
+package {'apache':
+name => $apache,
+ensure => latest,
+}
